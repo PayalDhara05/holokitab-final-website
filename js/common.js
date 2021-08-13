344cd37971bba4eba7017177442b1d62b0039df9
PayalDhara05 <payaldhara05@gmail.com>
2021-08-13 12:29:30 +0530

    all files added

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$(function()
+{
+    $("#Navbar").load("header.html");
+    $("#footer").load("footer.html");
+});
+
+AOS.init(
+    {
+        offset:200,
+        duration:1000
+    }
+  );
---
+++ js/index.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper1", {
+    spaceBetween: 30,
+    loop:true,
+            autoplay:true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+  
+
